69b81688383f8cdaed44e63cb80fba7518956a40
AlbinC08 <cedric.albin13780@gmail.com>
2022-10-30 18:42:34 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+        <Logo />
+        <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+        magnam eaque est quis dignissimos laborum culpa incidunt omnis amet esse
+        repellat maiores earum iure animi. Aperiam sapiente delectus minus eius
+        culpa corporis, ducimus fugit laboriosam nemo numquam odio architecto
+        vel quod labore provident quas inventore. Dolorem, consectetur adipisci?
+        Quas cumque odit iure iusto reprehenderit, exercitationem nulla
+        laudantium? Accusamus, aperiam ipsam fugit eveniet quam minus placeat
+        perspiciatis inventore dignissimos explicabo ipsum sint doloribus culpa
+        ratione, corporis, maiores a aliquam quod rerum harum iure. Iure
+        assumenda id aliquam sapiente molestiae, sequi dolorum minus cum
+        aspernatur sunt ex! Debitis voluptatem libero in veniam.
+      </p>
+    </div>
+  );
+};
+
+export default About;
